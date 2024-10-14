4b2f08146d1d5380a8be834880160add6abefd1f
Light-MR <lmaria.reyes221b@ciencias.unam.mx>
2024-10-14 17:51:57 -0600

    postgres

--- Ciencias_Top/Ciencias_Top/settings.py
+++ Ciencias_Top/Ciencias_Top/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'Ciencias_Top.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'CienciasTop',
+        'USER': 'admin',
+        'PASSWORD': 'admin1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+
 }
 
 
