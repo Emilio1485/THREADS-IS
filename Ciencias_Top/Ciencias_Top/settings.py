b5d2eca5cae96b3d9ae3173ed6890cc386abccf8
Light-MR <lmaria.reyes221b@ciencias.unam.mx>
2024-10-20 23:53:31 -0600

    modelo rentas

--- Ciencias_Top/Ciencias_Top/settings.py
+++ Ciencias_Top/Ciencias_Top/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'usuarios',
     'productos',
+    'rentas',
 ]
 
 MIDDLEWARE = [
---
+++ Ciencias_Top/rentas/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.conf import settings
+import uuid
+
+class Renta(models.Model):
+    id_renta = models.CharField( max_length=10, primary_key=True, verbose_name='Identificador de renta', editable=False)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='rentas', verbose_name='Usuario')
+    producto = models.ForeignKey('productos.Producto', on_delete=models.CASCADE, related_name='rentas', verbose_name='Producto rentado')
+    fecha_renta = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de renta')
+    fecha_devolucion_estimada = models.DateTimeField(verbose_name='Fecha de devolución estimada' )
+    fecha_devolucion = models.DateTimeField(blank=True, null=True, verbose_name='Fecha de devolución')
+    estado = models.CharField(max_length=20, choices=[('R', 'Rentado'), ('D', 'Devuelto'), ('T', 'Devuelto tarde'), ('P', 'Sin devolve aun')], default='R', verbose_name='Estado de la renta')
+    
+    pumapuntos_obtenidos = models.PositiveIntegerField(default=0, verbose_name='PumaPuntos obtenidos de esta renta')
+    
+    def save(self, *args, **kwargs):
+        if not self.id_renta:
+            self.id_renta = self.generar_id()
+            self.pumapuntos_obtenidos = self.producto.pumapuntos // 2
+        super(Renta, self).save(*args, **kwargs)
+        
+    
+    def generar_id(self):
+        """
+        Genera un identificador único para la renta.
+        """
+        return f'R-{uuid.uuid4().hex[:5].upper()}'
+    
+    
+    
+    def registrar_devolucion(self, fecha_devolucion_real):
+        """
+        Registra la fecha en la que el producto es devuelto y actualiza el estado.
+        """
+        self.fecha_devolucion = fecha_devolucion_real
+        if self.fecha_devolucion > self.fecha_devolucion_estimada:
+            self.estado = 'Tarde'
+        else:
+            self.estado = 'Devuelto'
+        self.save()
+    
+    def __str__(self):
+        return f'{self.id_renta} -> {self.usuario} -> {self.producto}'
+    
+    
+    
